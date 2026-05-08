f06b307c1db444161f8f0a7f1f7474096966d9c8
Eman ZainAldeen <30460456+EmanZainAldeen@users.noreply.github.com>
2026-05-08 17:30:27 +0300

    Start phase 1 split: move DB config to env-backed config

--- Graduation/dbConnection.php
+++ Graduation/dbConnection.php
@@ -1,10 +1,19 @@
 <?php
-$host ="localhost";
-$user ="root";
-$passward ="12345";
-$database = "Basma";
-$conn=mysqli_connect($host,$user,$passward,$database);
-if(!$conn){
-    die("connection failed: ".mysqli_connect_error());
+
+$dbConfig = require __DIR__ . '/../config/database.php';
+
+$host = $dbConfig['host'];
+$user = $dbConfig['user'];
+$passward = $dbConfig['password'];
+$database = $dbConfig['name'];
+$port = (int) $dbConfig['port'];
+
+$conn = mysqli_connect($host, $user, $passward, $database, $port);
+
+if (!$conn) {
+    die('connection failed: ' . mysqli_connect_error());
 }
-?>
+
+if (!empty($dbConfig['charset'])) {
+    mysqli_set_charset($conn, $dbConfig['charset']);
+}
